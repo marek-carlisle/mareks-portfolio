b51ed8bfa0bfe9399179f4ca4ef2753ad57717b4
Sevisis Maverick <Mavericksevisis@gmail.com>
2020-12-09 11:25:13 -0600

    Tryna deploy to Heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const portfolioProjectRouter = require('./routes/portfolioProject.router');
+// const portfolioProjectRouter = require('./routes/portfolioProject.router');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
